9e2cd2509a5ed9b5a3299096a7c2af4f12b9fe26
meltdeer <wangrato@qq.com>
2023-06-03 20:33:13 +0800

    blog: update setup_env.sh
    User can use this script to initialize build environment.
    
    Prerequirts:
    	1. npm
    	2. pnmp

--- setup_env.sh
+++ setup_env.sh
@@ -1,12 +1,18 @@
 #! /bin/bash
 
 # 使用包管理器初始化
- yarn init -y
+ pnmp init -y
 
- #将 VuePress 安装为本地依赖
- yarn add -D vuepress 
+# 构建 navigation
+# pnpm run docs:nav -f
+
+# 构建开发版本
+# pnpm run docs:dev
+
+#构建发布版本(如果使用 github action自动部署，local无需此步骤)
+# pnpm run docs:build
 
 # 为 vuepress 自动生成侧边栏的插件
 # https://shanyuhai123.github.io/vuepress-plugin-auto-sidebar/zh/#%E4%BB%8B%E7%BB%8D
- npm i vuepress-plugin-auto-sidebar -D
+# npm i vuepress-plugin-auto-sidebar -D
 
